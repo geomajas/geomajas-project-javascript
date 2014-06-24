81c958474fe84a518a44f67df947c1ec45de510b
janv <jan.venstermans@dfc.be>
2014-06-24 14:56:03 +0200

    checkstyle

--- server-extension-api/src/main/java/org/geomajas/javascript/api/client/map/layer/JsVectorServerLayer.java
+++ server-extension-api/src/main/java/org/geomajas/javascript/api/client/map/layer/JsVectorServerLayer.java
@@ -11,7 +11,6 @@
 package org.geomajas.javascript.api.client.map.layer;
 
 import org.geomajas.annotation.Api;
-import org.geomajas.javascript.api.client.map.layer.JsFeaturesSupported;
 
 /**
  * Exports {@link org.geomajas.gwt2.client.map.layer.VectorServerLayer}.
